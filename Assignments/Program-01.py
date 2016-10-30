2759362bbfb6ac2169fd981e8e360244c3280a83
Lyte195 <ldinh195@gmail.com>
2016-10-30 17:27:10 -0500

    Update Program-01.py
    Indentation of comment  blocks error fixed

--- Assignments/Program-01.py
+++ Assignments/Program-01.py
@@ -1,8 +1,9 @@
 """
-@ Luong Dinh
-@ Name: ShiftCipher
-@ Description: Simple class to do a shift cipher
+Luong Dinh
+ShiftCipher
+Simple class to do a shift cipher
 """
+
 AlphaNumeric = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9']
 
 class ShiftCipher(object):
@@ -13,24 +14,28 @@ class ShiftCipher(object):
 	@ Params:
 	     None
 	"""
+
 	def __init__(self):
 		
 		self.plainText = None
 		self.cipherText = None
 		self.cleanText = None
 		self.shift = 3
+
 	"""
 	Nice string representation of your class to help debug.
 	"""
+
 	def __str__(self):
 		return "plainText: %s\ncipherText: %s\ncleanText: %s\nshift: %d\n" % (self.plainText,self.cipherText,self.cleanText,self.shift)
-	
+
 	"""
 	@ Name: promptUserMessage
 	@ Description: Prompts user for message from standard in
 	@ Params:
 	     None
 	"""
+
 	def promptUserMessage(self):
 		temp = input("Message: ")
 		self.setMessage(temp)
@@ -42,6 +47,7 @@ class ShiftCipher(object):
 	     message {string}: String message
 	     encrypted {bool}: False = plaintext True=ciphertext
 	"""
+
 	def setMessage(self,message,encrypted=False):
 		if(not encrypted):
 			self.plainText = message
@@ -70,32 +76,29 @@ class ShiftCipher(object):
 				continue
 			if ord(letter) > 96:
 				self.cleanText += chr(ord(letter)-32)
-			else:
-
-	
 
 	"""
 	Encrypts plaintext not ciphertext
 	"""
+
 	def __encrypt(self):
 		self.cipherText = ''
 		if(not self.cleanText):
 			return
 		for letter in self.cleanText:
-		    self.cipherText += chr((((ord(letter)-65) + self.shift) % 36)+65)
-		    
-	
-	
+			self.cipherText += chr((((ord(letter)-65) + self.shift) % 36)+65)
+
 	"""
 	Decrypts ciphertext not plaintext
 	"""
+
 	def __decrypt(self):
 		self.cleanText =  self.cipherText
 		self.cipherText = ''
 		if(not self.cleanText):
 			return
 		for letter in self.cleanText:
-		    self.cipherText += chr((((ord(letter)-65) - self.shift) % 36)+65)
+			self.cipherText += chr((((ord(letter)-65) - self.shift) % 36)+65)
 
 """
 Only run this if we call this file directly:
