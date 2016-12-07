82f7ef44ba3f64391cdaa2c0aab5273b209997df
Lyte195 <ldinh195@gmail.com>
2016-12-06 22:59:18 -0600

    Update imageEdit.py

--- Assignments/program_3/imageEdit.py
+++ Assignments/program_3/imageEdit.py
@@ -1,24 +1,111 @@
 import sys
+from PIL import Image
+import random
+from random import randint
 
 class ImageEdit(object):
-    def __init__(self):
-        pass
-    def glassEffect(self,image,imageout=None):
-        pass
+    def __init__(self,image):
+        self.width =  image.size[0]
+        self.height = image.size[1]
+        self.image = image
+        
+    def glassEffect(self,image,distance=5,imageout=None):
+        i = randint(0,self.width)
+        nums = [x for x in range(i - distance, i + distance) if 0 <= x <= self.width]
+        
+        for h in range(0,self.width):
+            for w in range(0, self.height):
+                choice = random.choice(nums)
+                xPos = h
+                yPos = w              
+                newX = (xPos - choice) + (xPos + choice)
+                newY = (yPos - choice) + (yPos + choice)
+                color = image.getpixel((h,w))
+                image.putpixel((newX,newY),color)
+           
     
     def flip(self,image,imageout=None):
-        print("flipping",image,imageout)
+        for h in range(0, self.width):
+          for w in range(0, self.height):
+            opposite = (self.height - w)
+            color = image.getpixel((h,opposite))
+            image.putpixel((h,opposite),color)
+
+
         
-    def blur(self,image,imageout=None):
-        pass
-    def posterize(self,image,imageout=None):
-        print("postering")
-        
-    def solarize(self,image,imageout=None):
-        print("sostering")
+    def blur(self,image,blur_lvl = 5,imageout=None):
+        r = 0
+        g = 0
+        b = 0
+        d = 2*blur_lvl * 2*blur_lvl
+        for x in range(blur_lvl,self.width - blur_lvl):
+          for y in range(blur_lvl,self.height - blur_lvl):
+            for i in range(-blur_lvl,blur_lvl):
+              for j in range(-blur_lvl,blur_lvl):
+                pix = image.getpixel((x+i,y+j))
+                r += pix[0]
+                g += pix[1]
+                b += pix[2]
+            image.putpixel((x,y),(int(r/d),int(g/d),int(b/d)))
+            r = 0
+            g = 0 
+            b = 0
+
+
+    def posterize(self,image,snap_val,imageout=None):
+        for h in range(0,self.width):
+            for w in range(0, self.height):
+                r, g, b = image.getpixel((h,w))
+
+                newR = r % snap_val
+                newG = g % snap_val
+                newB = b % snap_val
+
+                if newR < (snap_val // 2):
+                    r -= newR
+                else:
+                    r += (snap_val - newR)
+
+                if newG < (snap_val // 2):
+                    g -= newG
+                else:
+                    g += (snap_val - newG)
+
+                if newB < (snap_val // 2):
+                    b -= newB
+                else:
+                    b += (snap_val - newB)
+
+                color = (r,g,b)
+                image.putpixel((h,w), color)
+   
+    def solarize(self,image,intensity=1,imageout=None):
+        for h in range(0,self.width):
+            for w in range(0, self.height):
+                r, g, b = image.getpixel((h,w))
+
+                if r < intensity:
+                    r = intensity - r
+                else:
+                    r += intensity
+
+                if g < intensity:
+                    g = intensity - g
+                else:
+                    g += intensity
+
+                if b < intensity:
+                    b = intensity - b
+                else:
+                    b += intensity
+
+                color = (r,g,b)
+                image.putpixel((h,w), color)
         
     def warhol(self,image,imageout=None):
         print("poslering")
+    
+
         
 print(sys.argv)
 
