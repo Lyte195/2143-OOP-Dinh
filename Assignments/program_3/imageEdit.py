9c65aa84f7ca7d28738ceccac5118a9fad81e887
Lyte195 <ldinh195@gmail.com>
2016-12-09 00:11:33 -0600

    Update imageEdit.py

--- Assignments/program_3/imageEdit.py
+++ Assignments/program_3/imageEdit.py
@@ -2,38 +2,73 @@ import sys
 from PIL import Image
 import random
 from random import randint
-
+"""
+@Luong Dinh
+@Class: ImageEdit
+@Description:
+    Represent an image with height and width
+@Methods
+    glassEffect - make image have glass effect
+    flip - flip image vertical
+    blur - make image blur
+    posterize - posterize image
+    solarize - solarize image
+    warhol - make image have warhol effect
+"""
 class ImageEdit(object):
     def __init__(self,image):
         self.width =  image.size[0]
         self.height = image.size[1]
-        self.image = image
+
+    """
+    @Method: glassEffect
+    @Description:
+        Pick a random pixel
+        Pick 2 nother pixel from 5 position up and down from pixel 1
+        Then add those 2 together
+        Then assign ^ this value to the first pixel picked
+    """
         
-    def glassEffect(self,image,distance=5,imageout=None):
+    def glassEffect(self,image,distance = 5):
         i = randint(0,self.width)
-        nums = [x for x in range(i - distance, i + distance) if 0 <= x <= self.width]
+        nums = [x for x in range(i - distance, i + distance) if x >= 0]
         
-        for h in range(0,self.width):
-            for w in range(0, self.height):
+        for w in range(0,self.width):
+            for h in range(0, self.height):
                 choice = random.choice(nums)
-                xPos = h
-                yPos = w              
+                xPos = w
+                yPos = h              
                 newX = (xPos - choice) + (xPos + choice)
                 newY = (yPos - choice) + (yPos + choice)
                 color = image.getpixel((h,w))
                 image.putpixel((newX,newY),color)
+
+        return image
            
-    
-    def flip(self,image,imageout=None):
-        for h in range(0, self.width):
-          for w in range(0, self.height):
+    """
+    @Method: flip
+    @Description:
+        Flip the picture vertically by loop through entire image one row at a time:
+        exchange current row with opposite row 
+
+    """    
+    def flip(self,image):
+        for w in range(0, self.width):
+          for h in range(0, self.height):
             opposite = (self.height - w)
             color = image.getpixel((h,opposite))
             image.putpixel((h,opposite),color)
 
+        return image
 
+    """
+    @Method: blur
+    @Description:
+        Blur the image give, this function was given in 
+        class.
+    """
         
-    def blur(self,image,blur_lvl = 5,imageout=None):
+    def blur(self,image,blur_lvl = 5):
         r = 0
         g = 0
         b = 0
@@ -50,12 +85,20 @@ class ImageEdit(object):
             r = 0
             g = 0 
             b = 0
+        
+        return image
 
+    """
+    @Method: posterize
+    @Description:
+        Posterize an image using a snap value to test and 
+        reducing the number of colors present in an image.
 
-    def posterize(self,image,snap_val,imageout=None):
-        for h in range(0,self.width):
-            for w in range(0, self.height):
-                r, g, b = image.getpixel((h,w))
+    """
+    def posterize(self,image,snap_val):
+        for w in range(0,self.width):
+            for h in range(0, self.height):
+                r, g, b = image.getpixel((w,h))
 
                 newR = r % snap_val
                 newG = g % snap_val
@@ -77,12 +120,22 @@ class ImageEdit(object):
                     b += (snap_val - newB)
 
                 color = (r,g,b)
-                image.putpixel((h,w), color)
+                image.putpixel((w,h), color)
+
+        return image
+
+    """
+    @Method: solarize
+    @Description:
+        Solarize and image by partial negation of the image
+        using an intensity value to test each color value
+        against the intensity the adjust the color occordingly.
+    """
    
-    def solarize(self,image,intensity=1,imageout=None):
-        for h in range(0,self.width):
-            for w in range(0, self.height):
-                r, g, b = image.getpixel((h,w))
+    def solarize(self,image,intensity=1):
+        for w in range(0,self.width):
+            for h in range(0, self.height):
+                r, g, b = image.getpixel((w,h))
 
                 if r < intensity:
                     r = intensity - r
@@ -100,33 +153,44 @@ class ImageEdit(object):
                     b += intensity
 
                 color = (r,g,b)
-                image.putpixel((h,w), color)
-        
-    def warhol(self,image,imageout=None):
-        print("poslering")
-    
+                image.putpixel((w,h), color)
 
-        
-print(sys.argv)
+        return image
 
-flags = ['-i','-o','-x','-h']
+    """
+    @Method: warhol
+    @Description:
+        Given warhol effect to an image by making a coppy
+        of the original image then gray scaling it.
+        Then differnt color values were added in each pixel
+        if the shade of red is at a certain threshold.
+    """    
+    def warhol(self,image):
+      
 
-commands = {}
+        newImage = image.copy()
+        orange = (255,165,0)
+        green = (0,255,0)
+        blue = (0,0,255)
+        pink = (255,192,203)
+        yellow = (255,255,0)
 
-for i in range(1,len(sys.argv),2):
-    commands[sys.argv[i]] = sys.argv[i+1]
+        for w in range(0,self.width):
+            for h in range (0, self.height):
+                r, g, b = image.getpixel((w, h))
+                grayscale = int((r+g+b)//3)
+                newImage.putpixel((w, h), (grayscale, grayscale, grayscale))
+                pixel = newImage.getpixel((w,h))      
 
-commands['-x'] = commands['-x'].split(',')
-print(commands)
-ie = ImageEdit()
+                if (r >0 and r <= 51):
+                    newImage.setColor(pixel,blue)
+                elif (r > 51 and r <= 102):
+                    newImage.setColor(pixel,yellow)
+                elif (r > 102 and r <= 153):
+                    newImage.setColor(pixel,orange)      
+                elif (r > 153 and r <= 204):
+                    newImage.setColor(pixel,pink)
+                else:
+                    newImage.setColor(pixel,green)     
 
-if '-i' in commands.keys():
-    input_file = commands['-i']
-    
-if '-o' in commands.keys():
-    output_file = commands['-o']
-else:
-    output_file = commands['-i']
-
-if commands['-x'][0] == 'flip':
-    ie.flip(input_file,output_file) 
+        return newImage
